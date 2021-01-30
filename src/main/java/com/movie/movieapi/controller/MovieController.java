56721b49eaaf41230e4f1aaf6726a4b3bfe9a956
isaiahk1116 <i.a.khan308@gmail.com>
2021-01-30 12:04:22 -0500

    Added comments

--- src/main/java/com/movie/movieapi/controller/MovieController.java
+++ src/main/java/com/movie/movieapi/controller/MovieController.java
@@ -15,20 +15,24 @@ public class MovieController {
     @Autowired
     private MoviesService moviesService;
 
+    // HTTP GET Request will return a list of movies
     @GetMapping
     public List<Movie> getMovies() {
         return moviesService.getMovies();
     }
 
+    // HTTP GET Request with an id in the path will return the corresponding movie
     @GetMapping(value ="{id}")
     public Movie getMovieById(@PathVariable long id) { return moviesService.getMovie(id); }
 
+    // HTTP POST Request accepts object in the request body and adds the object to the database. Returns a string
     @PostMapping
     public String saveMovie(@RequestBody Movie newMovie) {
         Movie movie = moviesService.addMovie(newMovie);
         return movie != null ? "Success" : "Failure";
     }
 
+    // HTTP DELETE Request with an id in the path will delete the corresponding movie from the database. Returns a string
     @DeleteMapping(value = "{id}")
     public String deleteMovie(@PathVariable long id) {
         if (moviesService.movieExists(id)) {
@@ -38,6 +42,8 @@ public class MovieController {
             return "Failure";
     }
 
+    // HTTP PUT Request with an id in the path accepts a movie object in the request
+    // body and updates movie corresponding to the provided id. Returns a string
     @PutMapping(value= "{id}")
     public String updateMovie(@PathVariable long id, @RequestBody Movie newMovie) {
         if (moviesService.movieExists(id)) {
